6116d8b70c063b0d2f3476b01d8d26708a8c9d6c
mithun.s <mithuns239@gmail.com>
2018-02-01 22:01:23 +0530

    make it better

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 
-http.listen(3000, function(){
+http.listen(process.env.PORT || 3000, function(){
   console.log('listening on *:3000');
 });
 
